6d76952b2821230046c22b49f24364ca400acb31
fernandesleticia <leticiafernandes777@gmail.com>
2019-04-19 23:57:35 -0300

    adiciona struct Task

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # todo-list
 TO-DO LIST With GraphQL Using Golang
+
+##dependencies
+go get github.com/graphql-go/graphql
---
+++ Task.go
@@ -0,0 +1,8 @@
+
+
+type Task struct {
+	ID    string `json:"id,omitempty"`
+	Title string `json:"title"`
+	Date  string `json:"date"`
+	Text  string `json:"text"`
+}
