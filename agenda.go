9673a6ccd2443c21bcf8936096a567c4beb69195
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-02-05 18:56:10 -0300

    create healthz methos

--- agenda.go
+++ agenda.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"io"
+	"net/http"
+
 	log "github.com/sirupsen/logrus"
 )
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	log.Info("All good with Agenda API")
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `I'am alive`)
+}
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
