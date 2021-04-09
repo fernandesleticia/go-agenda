c89fa86fe16d46e77191d2f675a143403f9a4bb3
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-04-09 18:10:16 -0300

    adding test for get done items

--- usecase/item_usecase_test.go
+++ usecase/item_usecase_test.go
@@ -1,24 +1,23 @@
 package usecase
 
-func TestFetch(t *testing.T) {
+func TestGetDoneItems(t *testing.T) {
 	mockItemRepo := new(mocks.Item)
 	var mockItem models.Item
 	err := faker.FakeData(&mockItem)
 	assert.NoError(t, err)
 
-	mockListArtilce := make([]*models.Item, 0)
-	mockListArtilce = append(mockListArtilce, &mockItem)
-	mockItemRepo.On("Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64")).Return(mockListArtilce, nil)
+	mockListItem := make([]*models.Item, 0)
+	mockListItem = append(mockListItem, &mockItem)
+	mockItemRepo.On("Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64")).Return(mockListItem, nil)
 	u := ucase.NewItemUsecase(mockItemRepo)
 	num := int64(1)
-	cursor := "12"
+	description := "12"
 	list, nextCursor, err := u.Fetch(cursor, num)
 	cursorExpected := strconv.Itoa(int(mockItem.ID))
 	assert.Equal(t, cursorExpected, nextCursor)
 	assert.NotEmpty(t, nextCursor)
 	assert.NoError(t, err)
-	assert.Len(t, list, len(mockListArtilce))
+	assert.Len(t, list, len(mockListItem))
 
 	mockItemRepo.AssertCalled(t, "Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64"))
-
 }
